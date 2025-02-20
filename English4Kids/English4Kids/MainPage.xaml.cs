5f737c77e9c2278d50788d63776959d6bc354947
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-20 19:32:56 -0300

    Aula 19/02/25

---
+++ English4Kids/English4Kids/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace English4Kids
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageCores());
+        }
+
+        private void btnFamilia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageFamilia());
+        }
+
+        private void btnNumeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageNumeros());
+        }
+
+        private void btnSair_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
